479d9e99bd43df0f9b01bfea0e5fec17eba7127c
meminolgc <meminolgc@gmail.com>
2025-03-05 17:42:32 +0300

    Add project files.

---
+++ NetCoreAI.Project02_ApiConsumeUI/Controllers/CustomersController.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NetCoreAI.Project02_ApiConsumeUI.Dtos;
+using Newtonsoft.Json;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace NetCoreAI.Project02_ApiConsumeUI.Controllers
+{
+	public class CustomersController : Controller
+	{
+		private readonly IHttpClientFactory _httpClientFactory;
+
+		public CustomersController(IHttpClientFactory httpClientFactory)
+		{
+			_httpClientFactory = httpClientFactory;
+		}
+
+		public async Task<IActionResult> CustomerList()
+		{
+			var client = _httpClientFactory.CreateClient();
+			var responseMessage = await client.GetAsync("https://localhost:44382/api/Customers");
+			if(responseMessage.IsSuccessStatusCode)
+			{
+				var jsonData = await responseMessage.Content.ReadAsStringAsync();
+				var values = JsonConvert.DeserializeObject<List<ResultCustomerDto>>(jsonData);
+				return View(values);
+			}
+			return View();
+			
+		}
+
+		[HttpGet]
+		public IActionResult CreateCustomer()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> CreateCustomer(CreateCustomerDto customerDto)
+		{
+			var client = _httpClientFactory.CreateClient();
+			var jsonData = JsonConvert.SerializeObject(customerDto);
+			StringContent content = new StringContent(jsonData,Encoding.UTF8,"application/json");
+			var responseMessage = await client.PostAsync("https://localhost:44382/api/Customers", content);
+			if(responseMessage.IsSuccessStatusCode)
+			{
+				return RedirectToAction("CustomerList");
+			}
+			return View();
+		}
+
+		public async Task<IActionResult> DeleteCustomer(int id)
+		{
+			var client = _httpClientFactory.CreateClient();
+			var responseMessage = await client.DeleteAsync("https://localhost:44382/api/Customers?id=" + id);
+			if (responseMessage.IsSuccessStatusCode)
+			{
+				return RedirectToAction("CustomerList");
+			}
+			return View();
+		}
+
+		[HttpGet]
+		public async Task<IActionResult> UpdateCustomer(int id)
+		{
+			var client = _httpClientFactory.CreateClient();
+			var responseMessage = await client.GetAsync("https://localhost:44382/api/Customers/Getcustomer/?id=" + id);
+			if (responseMessage.IsSuccessStatusCode)
+			{
+				var jsonData = await responseMessage.Content.ReadAsStringAsync();
+				var values = JsonConvert.DeserializeObject<GetByIdCustomerDto>(jsonData);
+				return View(values);
+			}
+			return View();
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> UpdateCustomer(UpdateCustomerDto updateCustomerDto)
+		{
+			var client = _httpClientFactory.CreateClient();
+			var jsonData = JsonConvert.SerializeObject(updateCustomerDto);
+			StringContent stringContent = new StringContent(jsonData,Encoding.UTF8,"application/json");
+			var responseMessage = await client.PutAsync("https://localhost:44382/api/Customers", stringContent);
+			if (responseMessage.IsSuccessStatusCode)
+			{
+				return RedirectToAction("CustomerList");
+			}
+			return View();
+		}
+	}
+}
---
+++ NetCoreAI.Project03_RapidApi/Program.cs
@@ -0,0 +1,31 @@
+﻿using NetCoreAI.Project03_RapidApi.ViewModels;
+using Newtonsoft.Json;
+using System.Net.Http.Headers;
+
+
+
+
+var client = new HttpClient();
+List<ApiSeriesViewModel> apiSeriesViewModels = new List<ApiSeriesViewModel>();
+var request = new HttpRequestMessage
+{
+	Method = HttpMethod.Get,
+	RequestUri = new Uri("https://imdb-top-100-movies.p.rapidapi.com/series/"),
+	Headers =
+	{
+		{ "x-rapidapi-key", "3283d06647mshd151ddd45740f93p1f96dfjsn293e6be9b8e0" },
+		{ "x-rapidapi-host", "imdb-top-100-movies.p.rapidapi.com" },
+	},
+};
+using (var response = await client.SendAsync(request))
+{
+	response.EnsureSuccessStatusCode();
+	var body = await response.Content.ReadAsStringAsync();
+	apiSeriesViewModels = JsonConvert.DeserializeObject<List<ApiSeriesViewModel>>(body);
+	foreach(var series in apiSeriesViewModels)
+	{
+		Console.WriteLine(series.rank + "-" + series.title + "-Film Puanı" + series.rating + "-Yapım Yılı:" + series.year);
+	}
+}
+
+Console.ReadLine();
