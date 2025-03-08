dc33a82341a3c18b516148e4c0501a5e28c2b0b9
meminolgc <meminolgc@gmail.com>
2025-03-08 04:16:40 +0300

    Translate and Text to Speech

---
+++ NetCoreAI.Project10_TextToSpeech/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Speech.Synthesis;
+
+class Program
+{
+	private static async Task Main(string[] args)
+	{
+		SpeechSynthesizer speechSynthesizer = new SpeechSynthesizer();
+		speechSynthesizer.Volume = 100;
+		speechSynthesizer.Rate = -1;
+
+		Console.Write("Metni giriniz: ");
+		string input;
+		input = Console.ReadLine();
+
+		if(!string.IsNullOrEmpty(input))
+		{
+			speechSynthesizer.Speak(input);
+		}
+
+		Console.ReadLine();
+	}
+}
